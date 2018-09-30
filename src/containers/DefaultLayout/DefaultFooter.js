8ee739f489fe57321b164a660b21b50cbd006dac
Marco Marassi <info@marco-marassi.com>
2018-09-30 18:29:58 +0100

    Updated links in footer.

--- src/containers/DefaultLayout/DefaultFooter.js
+++ src/containers/DefaultLayout/DefaultFooter.js
@@ -1,11 +1,5 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
-
-const propTypes = {
-    children: PropTypes.node,
-};
-
-const defaultProps = {};
 
 class DefaultFooter extends Component {
     render() {
@@ -14,13 +8,33 @@ class DefaultFooter extends Component {
         const { children, ...attributes } = this.props;
 
         return (
-            <React.Fragment>
-                <span><a href="https://coreui.io">CoreUI</a> &copy; 2018 creativeLabs.</span>
-                <span className="ml-auto">Powered by <a href="https://coreui.io/react">CoreUI for React</a></span>
-            </React.Fragment>
+            <Fragment>
+                <span>
+                    <a
+                        href="https://github.com/thtg88/mmcms"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >mmCMS</a>
+                    {" "}
+                    &copy; 2018 Marco Marassi.
+                </span>
+                <span className="ml-auto">
+                    Powered by
+                    {" "}
+                    <a
+                        href="https://coreui.io/react"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >CoreUI for React</a>
+                </span>
+            </Fragment>
         );
     }
 }
-DefaultFooter.propTypes = propTypes;
-DefaultFooter.defaultProps = defaultProps;
+
+DefaultFooter.propTypes = {
+    children: PropTypes.node,
+};
+DefaultFooter.defaultProps = {};
+
 export default DefaultFooter;
