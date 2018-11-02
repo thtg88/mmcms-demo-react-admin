2679b52cca07a02406f70a544daab5c161d568d5
Marco Marassi <info@marco-marassi.com>
2018-11-02 22:53:24 +0000

    Refactored default footer to functional component.

--- src/containers/DefaultLayout/DefaultFooter.js
+++ src/containers/DefaultLayout/DefaultFooter.js
@@ -1,46 +1,32 @@
-import React, { Component, Fragment } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 
-class DefaultFooter extends Component {
-    render() {
-
-        // eslint-disable-next-line
-        const { children, ...attributes } = this.props;
-
-        return (
-            <Fragment>
-                <span>
-                    <a
-                        href="https://github.com/thtg88/mmcms"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >mmCMS</a>
-                    {" "}
-                    &copy; 2018
-                    {" "}
-                    <a
-                        href="https://www.marco-marassi.com"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >M. Marassi</a>.
-                </span>
-                <span className="ml-auto">
-                    Powered by
-                    {" "}
-                    <a
-                        href="https://coreui.io/react"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >CoreUI for React</a>
-                </span>
-            </Fragment>
-        );
-    }
-}
-
-DefaultFooter.propTypes = {
-    children: PropTypes.node,
-};
-DefaultFooter.defaultProps = {};
+const DefaultFooter = () => (
+    <>
+        <span>
+            <a
+                href="https://github.com/thtg88/mmcms"
+                target="_blank"
+                rel="noopener noreferrer"
+            >mmCMS</a>
+            {" "}
+            &copy; 2018
+            {" "}
+            <a
+                href="https://www.marco-marassi.com"
+                target="_blank"
+                rel="noopener noreferrer"
+            >M. Marassi</a>.
+        </span>
+        <span className="ml-auto">
+            Powered by
+            {" "}
+            <a
+                href="https://coreui.io/react"
+                target="_blank"
+                rel="noopener noreferrer"
+            >CoreUI for React</a>
+        </span>
+    </>
+);
 
 export default DefaultFooter;
