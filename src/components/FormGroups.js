f254824a20bad3a930077ca07074d659ad283f64
Marco Marassi <thtg88@users.noreply.github.com>
2019-03-17 09:06:14 +0000

    Added groups prop check in FormGroups component

--- src/components/FormGroups.js
+++ src/components/FormGroups.js
@@ -8,52 +8,124 @@ import {
 } from 'reactstrap';
 import FormInput from './FormInput';
 
-const FormGroups = ({ groups }) => groups.map((group, idx) => {
-    const {
-        checked,
-        className,
-        closeOnSelect,
-        dataAttributes,
-        dateFormat,
-        disabled,
-        emptyOption,
-        formFeedbackText,
-        formText,
-        inputClassName,
-        invalid,
-        key,
-        label,
-        locale,
-        multiple,
-        name,
-        onChange,
-        placeholder,
-        rows,
-        timeFormat,
-        type,
-        value,
-        values,
-        viewMode,
-    } = group;
+const FormGroups = ({ groups }) => {
+    if(
+        !groups
+        || !groups.length
+    ) {
+        return null;
+    }
 
-    if(type === 'checkbox') {
-        return (
-            <FormGroup key={key} check className={className}>
-                <Label check>
+    return groups.map((group, idx) => {
+        const {
+            checked,
+            className,
+            closeOnSelect,
+            dataAttributes,
+            dateFormat,
+            disabled,
+            emptyOption,
+            formFeedbackText,
+            formText,
+            inputClassName,
+            invalid,
+            key,
+            label,
+            locale,
+            multiple,
+            name,
+            onChange,
+            placeholder,
+            rows,
+            timeFormat,
+            type,
+            value,
+            values,
+            viewMode,
+        } = group;
+
+        if(type === 'checkbox') {
+            return (
+                <FormGroup key={key} check className={className}>
+                    <Label check>
+                        <FormInput
+                            checked={checked}
+                            className={inputClassName}
+                            dataAttributes={dataAttributes}
+                            disabled={disabled}
+                            invalid={invalid}
+                            name={name}
+                            onChange={onChange}
+                            rows={rows}
+                            type={type}
+                            value={value}
+                        />
+                        {label}
+                    </Label>
+                    {
+                        formText
+                            ? <FormText>{formText}</FormText>
+                            : null
+                    }
+                    {
+                        formFeedbackText
+                            ? <FormFeedback>{formFeedbackText}</FormFeedback>
+                            : null
+                    }
+                </FormGroup>
+            );
+        }
+
+        if(type === 'hidden') {
+            return (
+                <Fragment key={key}>
                     <FormInput
-                        checked={checked}
                         className={inputClassName}
                         dataAttributes={dataAttributes}
                         disabled={disabled}
                         invalid={invalid}
                         name={name}
                         onChange={onChange}
-                        rows={rows}
                         type={type}
                         value={value}
                     />
-                    {label}
-                </Label>
+                    {
+                        formText
+                            ? <FormText>{formText}</FormText>
+                            : null
+                    }
+                    {
+                        formFeedbackText
+                            ? <FormFeedback>{formFeedbackText}</FormFeedback>
+                            : null
+                    }
+                </Fragment>
+            );
+        }
+
+        return (
+            <FormGroup key={key} className={className}>
+                <Label htmlFor={name}>{label}</Label>
+                <FormInput
+                    className={inputClassName}
+                    closeOnSelect={closeOnSelect}
+                    dataAttributes={dataAttributes}
+                    dateFormat={dateFormat}
+                    disabled={disabled}
+                    emptyOption={emptyOption}
+                    invalid={invalid}
+                    locale={locale}
+                    multiple={multiple}
+                    name={name}
+                    onChange={onChange}
+                    placeholder={placeholder}
+                    rows={rows}
+                    timeFormat={timeFormat}
+                    type={type}
+                    value={value}
+                    values={values}
+                    viewMode={viewMode}
+                />
                 {
                     formText
                         ? <FormText>{formText}</FormText>
@@ -66,71 +138,8 @@ const FormGroups = ({ groups }) => groups.map((group, idx) => {
                 }
             </FormGroup>
         );
-    }
-
-    if(type === 'hidden') {
-        return (
-            <Fragment key={key}>
-                <FormInput
-                    className={inputClassName}
-                    dataAttributes={dataAttributes}
-                    disabled={disabled}
-                    invalid={invalid}
-                    name={name}
-                    onChange={onChange}
-                    type={type}
-                    value={value}
-                />
-                {
-                    formText
-                        ? <FormText>{formText}</FormText>
-                        : null
-                }
-                {
-                    formFeedbackText
-                        ? <FormFeedback>{formFeedbackText}</FormFeedback>
-                        : null
-                }
-            </Fragment>
-        );
-    }
-
-    return (
-        <FormGroup key={key} className={className}>
-            <Label htmlFor={name}>{label}</Label>
-            <FormInput
-                className={inputClassName}
-                closeOnSelect={closeOnSelect}
-                dataAttributes={dataAttributes}
-                dateFormat={dateFormat}
-                disabled={disabled}
-                emptyOption={emptyOption}
-                invalid={invalid}
-                locale={locale}
-                multiple={multiple}
-                name={name}
-                onChange={onChange}
-                placeholder={placeholder}
-                rows={rows}
-                timeFormat={timeFormat}
-                type={type}
-                value={value}
-                values={values}
-                viewMode={viewMode}
-            />
-            {
-                formText
-                    ? <FormText>{formText}</FormText>
-                    : null
-            }
-            {
-                formFeedbackText
-                    ? <FormFeedback>{formFeedbackText}</FormFeedback>
-                    : null
-            }
-        </FormGroup>
-    );
-});
+})
+};
 
 FormGroups.propTypes = {
     groups: PropTypes.arrayOf(PropTypes.shape({
