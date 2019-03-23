6e6c9ffb6cb90ff738ec988c27f5fccdbedba9b1
Marco Marassi <thtg88@users.noreply.github.com>
2019-03-23 15:48:35 +0000

    Added support for retry the lazy import of the views in routes

--- src/routes.js
+++ src/routes.js
@@ -1,28 +1,59 @@
 import { lazy } from 'react';
+import { noInternetConnectionNotification } from './helpers/toastNotification';
 
-const Profile = lazy(() => import('./views/Pages/Profile'));
-const Dashboard = lazy(() => import('./views/Dashboard'));
+const Profile = lazy(() => retry(() => import('./views/Pages/Profile')));
+const Dashboard = lazy(() => retry(() => import('./views/Dashboard')));
+
+// Image Category lazies...
+const ImageCategoryCreate = lazy(() => retry(() => import('./views/Resources/ImageCategories/Create')));
+const ImageCategoryEdit = lazy(() => retry(() => import('./views/Resources/ImageCategories/Edit')));
+const ImageCategoriesList = lazy(() => retry(() => import('./views/Resources/ImageCategories/List')));
 
 // Roles lazies...
-const Roles = lazy(() => import('./views/Resources/Roles/List'));
-const RoleEdit = lazy(() => import('./views/Resources/Roles/Edit'));
-const RoleCreate = lazy(() => import('./views/Resources/Roles/Create'));
+const RolesList = lazy(() => retry(() => import('./views/Resources/Roles/List')));
+const RoleEdit = lazy(() => retry(() => import('./views/Resources/Roles/Edit')));
+const RoleCreate = lazy(() => retry(() => import('./views/Resources/Roles/Create')));
 
 // Users lazies...
-const Users = lazy(() => import('./views/Resources/Users/List'));
-const UserEdit = lazy(() => import('./views/Resources/Users/Edit'));
-const UserCreate = lazy(() => import('./views/Resources/Users/Create'));
+const UsersList = lazy(() => retry(() => import('./views/Resources/Users/List')));
+const UserEdit = lazy(() => retry(() => import('./views/Resources/Users/Edit')));
+const UserCreate = lazy(() => retry(() => import('./views/Resources/Users/Create')));
 
 const routes = [
     { path: '/', exact: true, name: 'Home', component: Dashboard },
     { path: '/dashboard', exact: true, name: 'Dashboard', component: Dashboard },
     { path: '/me', exact: true, name: 'Profile', component: Profile },
-    { path: '/roles', exact: true,  name: 'Roles', component: Roles },
+    { path: '/image-categories', exact: true,  name: 'Roles', component: ImageCategoriesList },
+    { path: '/image-categories/create', exact: true,  name: 'Create', component: ImageCategoryCreate },
+    { path: '/image-categories/:id', exact: true, name: 'Edit Role', component: ImageCategoryEdit },
+    { path: '/roles', exact: true,  name: 'Roles', component: RolesList },
     { path: '/roles/create', exact: true,  name: 'Create', component: RoleCreate },
     { path: '/roles/:id', exact: true, name: 'Edit Role', component: RoleEdit },
-    { path: '/users', exact: true,  name: 'Users', component: Users },
+    { path: '/users', exact: true,  name: 'Users', component: UsersList },
     { path: '/users/create', exact: true,  name: 'Create', component: UserCreate },
     { path: '/users/:id', exact: true, name: 'Edit User', component: UserEdit },
 ];
 
 export default routes;
+
+/**
+ * Credits to https://dev.to/goenning/how-to-retry-when-react-lazy-fails-mb5
+ */
+const retry = (fn, retriesLeft = 5, interval = 1000) => {
+    return new Promise((resolve, reject) => {
+        fn()
+            .then(resolve)
+            .catch((error) => {
+                setTimeout(() => {
+                    if (retriesLeft === 1) {
+                        // maximum retries exceeded
+                        noInternetConnectionNotification();
+                        return;
+                    }
+
+                    // Passing on "reject" is the important part
+                    retry(fn, retriesLeft - 1, interval).then(resolve, reject);
+                }, interval);
+            });
+    });
+}
