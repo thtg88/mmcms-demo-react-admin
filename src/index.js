0811616df9ba66a921c5ec6c78b5b1b77c960531
Marco Marassi <thtg88@users.noreply.github.com>
2019-08-10 22:25:50 +0100

    Removed HMR from index

--- src/index.js
+++ src/index.js
@@ -19,13 +19,6 @@ function render(Component) {
 
 render(App);
 
-if(module.hot) {
-    module.hot.accept('./App', () => {
-        const NextApp = require('./App').default;
-        render(NextApp);
-    })
-}
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
