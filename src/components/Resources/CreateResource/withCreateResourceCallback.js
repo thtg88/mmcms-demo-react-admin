013d5b9d6b29cdf6c587237348f76416dc4eb586
Marco Marassi <thtg88@users.noreply.github.com>
2020-05-21 22:18:15 +0100

    withCreateResourceCallback !!!

--- src/components/Resources/CreateResource/withCreateResourceCallback.js
+++ src/components/Resources/CreateResource/withCreateResourceCallback.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import withCreateResource from './withCreateResource';
 
 const withCreateResourceCallback = (callback) => (ComponentToWrap) => {
-    const CreateCallbackHOC = (props) => {
-        const params = callback(props);
+    class CreateCallbackHOC extends Component {
+        constructor(props) {
+            super(props);
 
-        return withCreateResource(params)(ComponentToWrap);
+            this.component = this.getComponent(props);
+        }
+
+        getComponent(props) {
+            const params = callback(props);
+            const WrappedComponent = (props) => <ComponentToWrap {...props} />;
+            const FinalComponent = withCreateResource(params)(WrappedComponent);
+
+            return <FinalComponent {...props} />;
+        }
+
+        render() {
+            return this.component;
+        }
     };
 
     return CreateCallbackHOC;
